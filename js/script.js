653163fa787b56085c9c8d60f38d628406833209
Noteventryin <tony1730280546@gmail.com>
2021-08-12 20:02:16 -0400

    Updated for comments

--- js/script.js
+++ js/script.js
@@ -10,13 +10,13 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+//Quotes found from https://www.bookbub.com/blog/famous-book-quotes
 const quotes = [
   {
      quote: "It is only with the heart that one can see rightly; what is essential is invisible to the eye." ,
      source: '— Antoine de Saint-Exupéry', 
      citation: 'The Little Prince', 
-     year: 1943,
-     tags: 'fantasy' 
+     year: 1943, 
   },
   { quote: "There is some good in this world, and it’s worth fighting for.",
     source: '— J.R.R. Tolkien',
@@ -43,43 +43,34 @@ const quotes = [
 /***
  * `getRandomQuote` function
  * This function will randomly create a random number from 0 to the max based on the length of the quotes Array.
- * The random number generated is then used to return the quote at the same index  within the quotes Array.
- * the function returns a random quote object using the random number variable above and bracket notation on the quotes array.
+ * The random number generated is used to return the quote at the same index  within the quotes Array.
 ***/
-getRandomQuote=()=>{
-  const randomIndex = Math.floor(Math.random() * quotes.length)
-
-  return quotes[randomIndex]
+getRandomQuote = () => {
+  const RandomNum = Math.floor(Math.random() * quotes.length)
+  return quotes[RandomNum]
 }
-//use console.log to check my code 
+//use console.log to check my code.
+//used arrow function to randomly select quotes.
 
-/***S
+/***
  * `printQuote` function
+ * The returned quote object build a string of html and quote properties.
 ***/
 function printQuote(){
   const randomQuote = getRandomQuote()
-  let html = ``
-  html += `<p class="quote">${randomQuote.quote}</p>`
-  html += `<p class="source">${randomQuote.source}`
+  let htmlstring = ` <p class="quote">${randomQuote.quote}</p>
+                     <p class="source">${randomQuote.source}`
+   
   if (randomQuote.citation) {
-    html += `<span class="citation">${randomQuote.citation}</span> `
+    htmlstring += `<span class="citation">${randomQuote.citation}</span> `
   }
   if (randomQuote.year) {
-    html += `<span class="year"> ${randomQuote.year} </span>`
+    htmlstring += `<span class="year"> ${randomQuote.year} </span>`
   }
-  if (randomQuote.tags) {
-    html += `<span class="tags"> ${randomQuote.tags}</span>`
-  }
-
-  html += `</p>`
-
-
-  return document.getElementById('quote-box').innerHTML= html
-  
-  
+  htmlstring += `</p>`
+  return document.getElementById('quote-box').innerHTML= htmlstring
 }
-
-//Code run and tested
+//Code tested and quotes are printed to the screen each time click the 'Show another quote' button or refresh the browzer. 
 
 /***
  * click event listener for the print quote button
